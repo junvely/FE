242243652cbd9151cdf70fca75289d94cb90a896
v-va <poruch0915@gmail.com>
2023-05-31 20:44:26 +0900

    #4 [Design] 네비게이션 바 구현

---
+++ src/components/common/nav/Nav.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './nav.module.scss';
+import homeIcon from '../../../assets/svg/home.svg';
+import chattingIcon from '../../../assets/svg/chatting.svg';
+import mypageIcon from '../../../assets/svg/mypage.svg';
+import postIcon from '../../../assets/svg/post.svg';
+
+function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <Link to='/main'>
+          <li>
+            <img src={homeIcon} alt='home' className={styles.homeIcon} />
+          </li>
+        </Link>
+        <li>
+          <img
+            src={chattingIcon}
+            alt='chatting'
+            className={styles.chattingIcon}
+          />
+        </li>
+        <li>
+          <img src={mypageIcon} alt='mypage' className={styles.mypageIcon} />
+        </li>
+      </ul>
+      <button type='button' className={styles.post}>
+        <img src={postIcon} alt='post' />
+      </button>
+    </nav>
+  );
+}
+
+export default Nav;
