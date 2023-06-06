7d3cc3ac18ca71bc4ad73a89cf92b9bf65b37745
junvely <junvely97@gmail.com>
2023-06-07 04:56:38 +0900

    #64 [Feat] 메인 페이지 검색 결과가 없을 시 문구 및 홈으로 돌아가기 버튼 생성

--- src/contexts/SearchQueryContext.js
+++ src/contexts/SearchQueryContext.js
@@ -3,24 +3,31 @@ import { createContext, useMemo, useState } from 'react';
 const SearchQueryContext = createContext();
 
 function SearchQueryProvider({ children }) {
-  const [searchQuery, setSearchQuery] = useState({
+  const initialQuery = {
     page: '',
     size: '',
     keyword: '',
     sorting: '인기순',
     district: '',
-  });
+  };
+
+  const [searchQuery, setSearchQuery] = useState(initialQuery);
 
   const updateSearchQuery = newPayload => {
     setSearchQuery(newPayload);
+  };
+
+  const resetSearchQuery = () => {
+    setSearchQuery(initialQuery);
   };
 
   const searchQueryValue = useMemo(
     () => ({
       searchQuery,
       updateSearchQuery,
+      resetSearchQuery,
     }),
-    [searchQuery, updateSearchQuery],
+    [searchQuery, updateSearchQuery, resetSearchQuery],
   );
 
   return (
