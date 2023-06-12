5f205a7645d3f07e3d06da73994100b468c39634
junvely <junvely97@gmail.com>
2023-06-13 02:56:53 +0900

    #107 [Feat] 채팅방, 채팅 기능 구현 Co-authored-by: v-va <poruch0915@gmail.com>

--- src/apis/chatting.js
+++ src/apis/chatting.js
@@ -1,33 +1,36 @@
 import SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
 import { getCookie } from 'utils/cookies';
-import instance from './instance';
+import instance from './instance/instance';
 
-// SockJS 연결
 let stompClient = null;
-const sock = new SockJS('https://api.ohpick.shop/ws');
-stompClient = Stomp.over(() => sock);
 
-// 실시간 채팅 받기
-const connectChat = roomId => {
+// 채팅 연결
+const connectClient = (roomId, jsonParseChat) => {
+  const sock = new SockJS('https://api.ohpick.shop/ws');
+  stompClient = Stomp.over(() => sock);
   stompClient.connect(
     {
-      access_token: getCookie('access_token'),
+      header: {
+        access_token: getCookie('access_token'),
+      },
     },
-    function (frame) {
-      console.log('frame= 연결 성공');
-      stompClient.subscribe('/api/chat/room/1');
+    frame => {
+      console.log('frame 연결 성공 =', frame);
+      stompClient.subscribe(`/sub/chat/room/${roomId}`, jsonParseChat);
     },
   );
 };
 
 // 실시간 채팅 보내기
-const sendChat = () => {
+const sendChat = (roomId, sender, message) => {
   stompClient.send(
     '/pub/chat/message',
     {},
     JSON.stringify({
-      sender: JSON.parse(localStorage.getItem('sender')),
+      roomId,
+      sender,
+      message,
     }),
   );
 };
@@ -35,12 +38,11 @@ const sendChat = () => {
 // 채팅데이터 가져오기
 const getChattingData = async roomId => {
   try {
-    const { data } = await instance.get(`/api/chat/room/1`);
-    console.log(data);
+    const { data } = await instance.get(`/api/chat/room/${roomId}`);
     return data;
   } catch (err) {
     throw new Error(err);
   }
 };
 
-export { getChattingData, connectChat, sendChat };
+export { getChattingData, connectClient, sendChat };
