269d9501b45faca59c89c84bfc99555d41ff8a8a
junvely <junvely97@gmail.com>
2023-06-13 21:07:18 +0900

    #51 [Refactor] 리프레쉬토큰을 확인해주세요. 엑세스토큰을 갱신할 수 없습니다. 에러 문구 시 로그아웃 처리하기

--- src/apis/instance/instance.js
+++ src/apis/instance/instance.js
@@ -45,7 +45,11 @@ instance.interceptors.response.use(
 
   error => {
     const message = error.response.data;
-    if (message === '리프레쉬 토큰이 만료되어 로그인이 필요합니다.') {
+    if (
+      message === '리프레쉬 토큰이 만료되어 로그인이 필요합니다.' ||
+      message ===
+        '리프레쉬토큰을 확인해주세요. 엑세스토큰을 갱신할 수 없습니다.'
+    ) {
       removeCookie();
       alert('로그인 기간이 만료되어 로그인 페이지로 이동합니다.');
       window.location.href = '/login';
