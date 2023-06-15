93478bf7235ed00cda313c278be4bc6a0ecff42f
v-va <poruch0915@gmail.com>
2023-06-15 20:04:44 +0900

    #138 [Refactor] Recoil setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Router from 'shared/Router';
+import { RecoilRoot } from 'recoil';
 import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Router />
-    </AuthContextProvider>
+    <RecoilRoot>
+      <AuthContextProvider>
+        <Router />
+      </AuthContextProvider>
+    </RecoilRoot>
   );
 }
 
